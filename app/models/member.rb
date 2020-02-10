0b6a2dd7c10d8afc7167524b7e5aa27c2a3e1bb7
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-10 10:36:46 -0700

    Refactor Member PORO to look for string keys in member_info hash

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,9 +1,9 @@
 class Member
   attr_reader :name, :house, :patronus, :role
   def initialize(member_info)
-    @name = member_info[:name]
-    @house = member_info[:house]
-    @patronus = member_info[:patronus] || nil
-    @role = member_info[:role] || nil
+    @name = member_info['name']
+    @house = member_info['house']
+    @patronus = member_info['patronus'] || nil
+    @role = member_info['role'] || nil
   end
 end
