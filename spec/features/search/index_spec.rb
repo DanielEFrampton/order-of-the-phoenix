48542bc12af34abe8d4097a6497f0d0bae125716
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-10 09:34:36 -0700

    Create framework for feature test of search index

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor', type: :feature do
+  describe 'when I visit root, select Gryffindor, and click search' do
+    before(:each) do
+      visit '/'
+      select 'Gryffindor', from: 'house'
+      click_on 'Search For Members'
+    end
+
+    it 'Then I should be on page “/search”' do
+      expect(current_path).to eq('/search')
+    end
+
+    it 'Then I should see a total of the number of members for that house. (21 for Gryffindor)' do
+      expect(page).to have_content("Total Order Members In Gryffindor: 21")
+    end
+
+    describe 'And I should see a list of Gryffindor members, and for each I should see' do
+      it 'The name of the member' do
+        require "pry"; binding.pry
+        page.all('.member').each do |member|
+          require "pry"; binding.pry
+        end
+      end
+
+      it 'The name of the member' do
+      end
+
+      it 'The name of the member' do
+      end
+
+      it 'The name of the member' do
+      end
+
+      it 'The members role (if it exists)' do
+      end
+
+      it 'The house the member belongs to' do
+      end
+
+      it 'The Patronus of the member (if it exists)' do
+      end
+    end
+  end
+end
